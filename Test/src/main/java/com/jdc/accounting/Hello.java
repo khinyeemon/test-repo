c7d886048dc8a9695cd0718c985865bba0c66ca0
khinyeemon <101930379+khinyeemon@users.noreply.github.com>
2024-12-23 22:07:28 +0630

    Update Hello.java

--- Test/src/main/java/com/jdc/accounting/Hello.java
+++ Test/src/main/java/com/jdc/accounting/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("Hello");
 		System.out.println("Hello Khin");
 				System.out.println(" Khin Yee Mon");
-								System.out.println(" Yamin"); 
+								System.out.println(" Yamin hu"); 
 
 		
 
