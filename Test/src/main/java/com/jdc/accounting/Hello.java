49c71d751a3c875a0c67f2d1c40096409f7a8610
khinyeemon <101930379+khinyeemon@users.noreply.github.com>
2024-12-23 21:50:37 +0630

    Update Hello.java

--- Test/src/main/java/com/jdc/accounting/Hello.java
+++ Test/src/main/java/com/jdc/accounting/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("Hello");
 		System.out.println("Hello Khin");
 				System.out.println(" Khin Yee Mon");
-						System.out.println(" Thu THu ");
+						System.out.println(" Thu THu  Aung Khin"); 
 
 
 
